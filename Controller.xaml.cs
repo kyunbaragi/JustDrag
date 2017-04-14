39c60f0941d7894073d8d91782373fb3a1d98b28
Sikurity <leejk9592@naver.com>
2017-04-14 18:31:27 +0900

    Add Korean Trained Data

--- Controller.xaml.cs
+++ Controller.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace JustDrag
 {
@@ -24,7 +14,7 @@ namespace JustDrag
             InitializeComponent();
             
             string dataPath = @"C:/Users/Administrator/Documents/Visual Studio 2015/Projects/JustDrag/JustDrag/tessdata/";
-            string language = @"eng";
+            string language = @"kor";
 
             using (var api = new Tesseract.TessBaseAPI(dataPath, language))
             {
